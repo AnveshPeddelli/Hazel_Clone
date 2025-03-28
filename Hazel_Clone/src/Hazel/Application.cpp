7c14c85587f54ddb68d61a11294b37c2e3a0d15a
Anvesh Peddelli <p.anvesh@intechadditive.com>
2025-03-28 19:09:39 +0530

    Setup basic Application and Entry Point

---
+++ Hazel_Clone/src/Hazel/Application.cpp
@@ -0,0 +1,23 @@
+#include "Application.h"
+
+namespace Hazel
+{
+	Application::Application()
+	{
+
+	}
+	
+	Application::~Application()
+	{
+		
+	}
+	
+	void Application::Run()
+	{
+		std::cout << "Hazel Engine" << std::endl;
+		while (true)
+		{
+
+		}
+	}
+}
